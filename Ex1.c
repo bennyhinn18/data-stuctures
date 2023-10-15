ebea38cba63b40033d344ce468a5795cc22b92ce
Bennyhinn18 <137780675+Bennyhinn18@users.noreply.github.com>
2023-10-15 09:42:09 +0000

    Sun, Oct 15, 2023, 3:12 PM +05:30

--- Ex1.c
+++ Ex1.c
@@ -12,48 +12,50 @@ pop - to delete an element from the stack
 display – to display the elements of a stack.
 Exit – to exit from the program
 */
-#include<stdio.h>
-void main()
+#include <stdio.h>
+int main()
 {
-int a[10]={0},i,top=-1,max=10,n,x;
-printf("\n\tMENU\n1.PUSH\n2.POP\n3.DISPLAY\n4.EXIT\n");
-do
-{
-printf("\nEnter your choice\n");
-scanf("%d",&n);
-switch(n)
-{
-case 1:
-if(top==max-1)
-printf("Stack is FULL\n");
-else
-{
-printf("Enter the element\n");
-scanf("%d",&x);
-a[++top]=x;
-}
-break;
-case 2:
-if(top<0)
-printf("Stack is Empty\n");
-else
-printf("The deleted item =%d",a[top--]); break;
-case 3:
-if(top<0)
-printf("The stack is empty\n");
-else
-{
-printf("The elements of the stack are :");
-for(i=0;i<=top;i++)
-printf("%d\n",a[i]);
-}
-break;
-case 4:
-break;
-default:
-printf("Invalid choice\n");
-break;
-}
-}
-while(n!=4);
+    int a[10] = {0}, i, top = -1, max = 10, n, x;
+    printf("\n\tMENU\n1.PUSH\n2.POP\n3.DISPLAY\n4.EXIT\n");
+    do
+    {
+        printf("\nEnter your choice\n");
+        scanf("%d", &n);
+        switch (n)
+        {
+            case 1:
+                if (top == max - 1)
+                    printf("Stack is FULL\n");
+                else
+                {
+                    printf("Enter the element\n");
+                    scanf("%d", &x);
+                    a[++top] = x;
+                }
+                break;
+            case 2:
+                if (top < 0)
+                    printf("Stack is Empty\n");
+                else
+                    printf("The deleted item =%d", a[top--]);
+                break;
+            case 3:
+                if (top < 0)
+                    printf("The stack is empty\n");
+                else
+                {
+                    printf("The elements of the stack are :");
+                    for (i = 0; i <= top; i++)
+                        printf("%d\n", a[i]);
+                }
+                break;
+            case 4:
+                break;
+            default:
+                printf("Invalid choice\n");
+                break;
+        }
+    }
+    while (n != 4);
+    return 0;
 }
