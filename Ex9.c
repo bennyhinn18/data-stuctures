6816aeba97900403a39603950564633f3b5d0bab
Bennyhinn18 <137780675+Bennyhinn18@users.noreply.github.com>
2023-11-29 14:15:28 +0530

    Create Ex9.c

---
+++ Ex9.c
@@ -0,0 +1,99 @@
+Ex. No: 9 		IMPLEMENTATION OF DIJIKSTRA’S ALGORITHM
+AIM:
+To write a program to implementation of Dijkstra’s algorithm.
+ALGORITHM:
+1. start the program
+2. Initialize the required variables
+3. Assign to every node a distance value. Set it to zero for our initial node and to infinity for all other nodes.
+4. Mark all nodes as unvisited. Set initial node as current.
+5. For current node, consider all its unvisited neighbors and calculate their distance (from the initial node). For example, if current node (A) has distance of 6, and an edge connecting it with another node (B) is 2, the distance to B through A will be 6+2=8. If this distance is less than the previously recorded distance (infinity in the beginning, zero for the initial node), overwrite the distance.
+6. When we are done considering all neighbors of the current node, mark it as visited. A visited node will not be checked ever again; its distance recorded now is final and minimal.
+7. Set the unvisited node with the smallest distance (from the initial node) as the next "current node" and continue from step 3.
+PROGRAM:
+#include<stdio.h>
+#include<conio.h>
+#define INFINITY 9999
+#define MAX 10
+void dijkstra(int G[MAX][MAX],int n,int startnode);
+int main()
+{
+int G[MAX][MAX],i,j,n,u;
+printf("Enter no. of vertices:");
+scanf("%d",&n);
+printf("\nEnter the adjacency matrix:\n");
+for(i=0;i<n;i++)
+for(j=0;j<n;j++)
+scanf("%d",&G[i][j]);
+printf("\nEnter the starting node:");
+scanf("%d",&u);
+dijkstra(G,n,u);
+return 0;
+}
+void dijkstra(int G[MAX][MAX],int n,int startnode)
+{
+int cost[MAX][MAX],distance[MAX],pred[MAX];
+int visited[MAX],count,mindistance,nextnode,i,j;
+//pred[] stores the predecessor of each node
+//count gives the number of nodes seen so far
+//create the cost matrix
+for(i=0;i<n;i++)
+for(j=0;j<n;j++)
+if(G[i][j]==0)
+cost[i][j]=INFINITY;
+else
+cost[i][j]=G[i][j];
+//initialize pred[],distance[] and visited[]
+for(i=0;i<n;i++)
+{
+distance[i]=cost[startnode][i];
+pred[i]=startnode;
+visited[i]=0;
+}
+distance[startnode]=0;
+visited[startnode]=1;
+count=1;
+while(count<n-1)
+{
+mindistance=INFINITY;
+//nextnode gives the node at minimum distance
+for(i=0;i<n;i++)
+if(distance[i]<mindistance&&!visited[i])
+{
+mindistance=distance[i];
+nextnode=i;
+}
+//check if a better path exists through nextnode
+visited[nextnode]=1;
+for(i=0;i<n;i++)
+if(!visited[i])
+if(mindistance+cost[nextnode][i]<distance[i])
+{
+distance[i]=mindistance+cost[nextnode][i];
+pred[i]=nextnode;
+}
+count++;
+}
+//print the path and distance of each node
+for(i=0;i<n;i++)
+if(i!=startnode)
+{
+printf("\nDistance of node%d=%d",i,distance[i]);
+printf("\nPath=%d",i);
+j=i;
+do
+{
+j=pred[j];
+printf("<-%d",j);
+}while(j!=startnode);
+}
+}
+
+
+
+
+Output
+
+RESULT:
+
+Thus the C program to implement Dijikstra’s algorithm was written and executed
+successfully.
